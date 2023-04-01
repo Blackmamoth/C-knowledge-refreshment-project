32da9bb2e728b40f90341de67fbc6393566ac519
blackmamoth <blackmamoth83@gmail.com>
2023-04-01 17:09:38 +0530

    moved macros definition to student.h

--- headers/management.h
+++ headers/management.h
@@ -5,11 +5,6 @@
 #include <ctype.h>
 #include "student.h"
 
-#define FILE_NAME "students.csv"
-#define TEMP_FILE_NAME "tempFile.csv"
-#define FILE_HEADING "NAME, AGE, ROLL NO, GPA"
-#define MAX_LINE_LENGTH 1024
-
 void assignFileHeading();
 
 short checkFileExists();
--- headers/student.h
+++ headers/student.h
@@ -1,6 +1,10 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#define FILE_NAME "students.csv"
+#define TEMP_FILE_NAME "tempFile.csv"
+#define FILE_HEADING "NAME, AGE, ROLL NO, GPA"
+#define MAX_LINE_LENGTH 1024
 #define MAX_NAME_LENGTH 100
 
 typedef struct
